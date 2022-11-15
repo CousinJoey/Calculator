167e43dd68267b5f641d0091682575dbf17b2033
CousinJoey <cousinjoeyllc@gmail.com>
2022-11-15 17:29:13 -0500

    finished calculator design, added decimal, delete, and clear functions, and was able to allow two operators to compute an operation

--- script.js
+++ script.js
@@ -1,115 +1,181 @@
 
-finalArr = []
-initalArr = [0]
+// Variables that are used repetatively in functions 
+
 operatorToggle = false
 integerOne = []
 integerTwo = []
 operatorArr = []
 
+// adding all elements used to the DOM 
+
 const btns = document.querySelectorAll(".btn")
-const displayUpdate = document.querySelector(".lower-content")
+const displayLower = document.querySelector(".lower-content")
 const deletebtn = document.querySelector("#delete")
 const operatorBtns = document.querySelectorAll(".btn-op")
 const equalBtn = document.querySelector(".btn-eq")
 const addBtn = document.querySelector("#add")
 const displayUpper = document.querySelector(".upper-content")
-
-for (let i = 0; i < operatorBtns.length; i++) {
-    operatorBtns[i].addEventListener("click", allOpsFunc)
-    operatorBtns[i].addEventListener("click", storeOp)
-    /*operatorBtns[i].addEventListener('click', checkFunc)*/
-}
-
-function storeOp(e) {
-    let operator = e.target.id
-        if (operatorArr.length < 1) {
-            operatorArr.push(operator) 
-        } else if (operatorArr.length > 0) {
-            operatorArr.pop()
-            operatorArr.push(operator)
-        }
-    console.log(operatorArr)
-}
-
-function allOpsFunc(e) {
-    operatorToggle = true
-    integerTwo.splice(0, integerTwo.length)
-    displayUpper.textContent = (integerOne.join("") + e.target.id)
-    displayUpdate.textContent = 0
-}
-
-/*function checkFunc() {
-    if (operatorArr.includes(""))
-}*/
-
-
-equalBtn.addEventListener("click", operateFunc)
-
-function operateFunc() {
-    displayUpper.textContent = ""
-    if (operatorArr.includes("+")) {
-        operatorArr.pop()
-        addFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
-    } else if (operatorArr.includes("x")) {
-        operatorArr.pop()
-        multiplyFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
-    } else if (operatorArr.includes("/")) {
-        operatorArr.pop()
-        divideFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
-    } else if (operatorArr.includes("-")) {
-        operatorArr.pop()
-        subtractFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
-    }
-}
-
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", getID)
-}
-
+const clearBtn = document.querySelector("#clear")
+const decimal = document.querySelector(".btn-dec")
 const buttonAdd = document.querySelector("#add")
 
-displayUpdate.textContent = initalArr
+// events associated with buttons on the calculator, runs appropriate function 
 
-function getID(e) {
+equalBtn.addEventListener("click", operateFunc)
+clearBtn.addEventListener("click", clearAll)
+deletebtn.addEventListener("click", deleteInt)
+decimal.addEventListener("click", decimalAdd)
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", appendNumb)
+}
+
+function appendNumb(e) {
    number = (e.target.id)
-   if (operatorToggle == false) {
+   if ((operatorToggle == false) && (integerOne.length < 11)) {
     integerOne.push(number)
-    displayUpdate.textContent = integerOne.join("")
-   } else if (operatorToggle == true) {
+    displayLower.textContent = integerOne.join("")
+   } else if ((operatorToggle == true) && (integerTwo.length < 11)) {
     integerTwo.push(number)
-    displayUpdate.textContent = integerTwo.join("")
+    displayLower.textContent = integerTwo.join("")
    }
 }
 
+for (let i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].addEventListener("click", operatorToggleFunc)
+    operatorBtns[i].addEventListener("click", storeOp)
+}
+
+// main function for letting two operator actions call an operation
+// for example, hitting + while + is already stored in an operator will cause it just to give the sum of the two integers
+
+function storeOp(e) {
+    let operator = (e.target.id)
+        if ((integerTwo.length == 0)) {
+            operatorArr.pop()
+            operatorArr.push(operator) 
+        } else if ((integerTwo.length > 0)) {
+            operateFunc(operatorArr[0])
+            operatorArr.pop()
+            operatorArr.push(operator)
+            displayUpper.textContent = (integerOne.join("") + e.target.id)
+        } else if (operatorArr.length > 2) {
+            operatorArr.slice(0,2)
+        }
+}
+
+function operatorToggleFunc(e) {
+    operatorToggle = true
+    displayUpper.textContent = (integerOne.join("") + e.target.id)
+}
+
+// main function for computing operations, takes in values and calls appropriate function //
+
+
+function operateFunc() {
+    displayUpper.textContent = ""
+    if ((operatorArr.includes("+") && (integerTwo.length > 0))) {
+        addFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
+        displayUpper.textContent = (integerOne.join("") + "+")
+    } else if ((operatorArr.includes("x") && (integerTwo.length > 0))) {
+        multiplyFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
+        displayUpper.textContent = (integerOne.join("") + "x")
+    } else if ((operatorArr.includes("/") && (integerTwo.length > 0))) {
+        divideFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
+        displayUpper.textContent = (integerOne.join("") + "/")
+    } else if ((operatorArr.includes("-") && (integerTwo.length > 0))) {
+        subtractFunc(parseFloat(integerOne.join("")), parseFloat(integerTwo.join("")))
+        displayUpper.textContent = (integerOne.join("") + "-")
+    }
+}
+
+
 function subtractFunc(x,y) {
     integerOne.splice(0, integerOne.length)
-    integerOne.push(x-y)
+    integerOne.push(Math.round((x-y) * 1000) / 1000)
     integerTwo.splice(0, integerTwo.length)
-    displayUpdate.textContent = integerOne
+    displayLower.textContent = integerOne
 }
 
 function addFunc(x,y) {
     integerOne.splice(0, integerOne.length)
-    integerOne.push(x+y)
+    integerOne.push(Math.round((x+y) * 1000) / 1000)
     integerTwo.splice(0, integerTwo.length)
-    displayUpdate.textContent = integerOne
+    displayLower.textContent = integerOne
 }
 
 function multiplyFunc(x,y) {
     integerOne.splice(0, integerOne.length)
-    integerOne.push(x*y)
+    integerOne.push(Math.round((x*y) * 1000) / 1000)
     integerTwo.splice(0, integerTwo.length)
-    displayUpdate.textContent = integerOne
+    displayLower.textContent = integerOne
 }
 
 function divideFunc(x,y) {
     if (y == 0) {
-        displayUpdate.textContent = "Cannot divide by Zero"
+        displayLower.textContent = "Cannot divide by Zero"
         integerTwo.splice(0, integerTwo.length)
     } else {
         integerOne.splice(0, integerOne.length)
-        integerOne.push(x/y)
+        integerOne.push(Math.round((x/y) * 1000) / 1000)
         integerTwo.splice(0, integerTwo.length)
-        displayUpdate.textContent = integerOne
+        displayLower.textContent = integerOne
     }
+}
+
+function clearAll() {
+    operatorToggle = false
+    integerOne = []
+    integerTwo = []
+    operatorArr = []
+    displayLower.textContent = ""
+    displayUpper.textContent = ""
+}
+
+function deleteInt() {
+    if (operatorToggle == false) {
+        integerOne.pop()
+        displayLower.textContent = integerOne.join("")
+    } else if (operatorToggle == true) {
+        integerTwo.pop()
+        displayLower.textContent = integerTwo.join("")
+    }
+} 
+
+function decimalAdd (e) {
+    if ((operatorToggle == false) && (!integerOne.includes("."))) {
+        integerOne.push(e.target.id)
+        displayLower.textContent = integerOne.join("")
+    } else if ((operatorToggle == true) && (!integerTwo.includes("."))) {
+        integerTwo.push(e.target.id)
+        displayLower.textContent = integerTwo.join("")
+    }
+}
+
+// adds keyboard functionality, needed to use e.key to get element ID since that was the type of object used to initially
+// input the numbers into the integer arrays
+
+window.addEventListener("keydown", keyboardFunc)
+
+function keyboardFunc(e) {
+    e.preventDefault();
+    if (e.key === "0") document.getElementById("0").click()
+    if (e.key === "1") document.getElementById("1").click()
+    if (e.key === "2") document.getElementById("2").click()
+    if (e.key === "3") document.getElementById("3").click()
+    if (e.key === "4") document.getElementById("4").click()
+    if (e.key === "5") document.getElementById("5").click()
+    if (e.key === "6") document.getElementById("6").click()
+    if (e.key === "7") document.getElementById("7").click()
+    if (e.key === "8") document.getElementById("8").click()
+    if (e.key === "9") document.getElementById("9").click()
+    if (e.key === "Enter") document.getElementById("equals").click()
+    if (e.key === "+") document.getElementById("+").click()
+    if (e.key === "*") document.getElementById("x").click()
+    if (e.key === "/") document.getElementById("/").click()
+    if (e.key === "-") document.getElementById("-").click()
+    if (e.key === ".") document.getElementById(".").click()
+    if (e.key === "Backspace") document.getElementById("delete").click()
+    if (e.key === "Escape") document.getElementById("clear").click()
 }
